60e595f28275c814e75e84f37bda38e73d4f04fc
muchachooos <odnorogg@bk.ru>
2022-12-12 03:18:22 +0400

    Update user.go

--- handler/user.go
+++ handler/user.go
@@ -1,17 +1,41 @@
 package handler
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"net/http"
 )
 
-func (s *Server) Hand(context *gin.Context) {
-	context.Writer.WriteString("Oke")
-	fmt.Println("Ok")
-}
+func (s *Server) RegistrationHandler(context *gin.Context) {
 
-//Reg handler
+	log, ok := context.GetQuery("login")
+	if log == "" || !ok {
+		context.Status(http.StatusBadRequest)
+		context.Writer.WriteString("Login is missing")
+		return
+	}
+
+	pass, ok := context.GetQuery("password")
+	if pass == "" || !ok {
+		context.Status(http.StatusBadRequest)
+		context.Writer.WriteString("Password is missing")
+		return
+	}
+
+	token := uuid.NewString()
+
+	err := s.Storage.RegistrationUserInBD(log, pass, token)
+	if err != nil {
+		context.Status(500)
+		context.Writer.WriteString("Something went wrong. Try again")
+		return
+	}
+
+	context.Status(http.StatusOK)
+	context.Writer.WriteString("Welcome to the club Body")
+}
 
 //Login handler
 
 //Check handler
+
