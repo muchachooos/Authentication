53ff6de5dd80b6d0ae544de50484e9dcb11f454e
muchachooos <odnorogg@bk.ru>
2022-12-12 20:16:10 +0400

    Update user.go

--- handler/user.go
+++ handler/user.go
@@ -1,9 +1,12 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"net/http"
+	"time"
 )
 
 func (s *Server) RegistrationHandler(context *gin.Context) {
@@ -24,7 +27,9 @@ func (s *Server) RegistrationHandler(context *gin.Context) {
 
 	token := uuid.NewString()
 
-	err := s.Storage.RegistrationUserInBD(log, pass, token)
+	time := time.Now()
+
+	err := s.Storage.RegistrationUserInBD(log, pass, token, time)
 	if err != nil {
 		context.Status(500)
 		context.Writer.WriteString("Something went wrong. Try again")
@@ -35,7 +40,49 @@ func (s *Server) RegistrationHandler(context *gin.Context) {
 	context.Writer.WriteString("Welcome to the club Body")
 }
 
-//Login handler
+func (s *Server) AuthorizationHandler(context *gin.Context) {
+
+	log, ok := context.GetQuery("login")
+	if log == "" || !ok {
+		context.Writer.WriteString("No login")
+		return
+	}
+
+	pass, ok := context.GetQuery("password")
+	if pass == "" || !ok {
+		context.Writer.WriteString("No password")
+		return
+	}
+
+	token := uuid.NewString()
+
+	resultTable, isChanged, err := s.Storage.AuthorizationUserInDB(log, pass, token)
+	if err != nil {
+		context.Status(500)
+		fmt.Println("ERROR : ", err)
+		context.Writer.WriteString("Something went wrong. Try again")
+		return
+	}
+
+	if isChanged == false {
+		context.Writer.WriteString("Something went wrong")
+		return
+	}
+
+	if len(resultTable) == 0 {
+		context.Status(404)
+		context.Writer.WriteString("")
+		return
+	}
+
+	jsonInByte, err := json.Marshal(resultTable)
+	if err != nil {
+		context.Writer.WriteString("json creating error")
+		return
+	}
+
+	context.Writer.Write(jsonInByte)
+
+}
 
 //Check handler
-
