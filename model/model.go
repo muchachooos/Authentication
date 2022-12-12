9094cdc2f2aff9a916826ef7655b994b183ff017
muchachooos <odnorogg@bk.ru>
2022-12-12 20:16:20 +0400

    Update model.go

--- model/model.go
+++ model/model.go
@@ -1,5 +1,18 @@
 package model
 
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"time"
+)
+
+type Data struct {
+	ID       int       `db:"id" json:"id"`
+	Login    string    `db:"login" json:"login"`
+	Password string    `db:"password" json:"password"`
+	Token    string    `db:"token" json:"token"`
+	Time     time.Time `db:"time" json:"time"`
+}
+
 type Config struct {
 	DataSourceName string `json:"dataSourceName"`
 	Port           int    `json:"port"`
