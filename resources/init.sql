fe3545b5185d7e15a0c5735fc17547595c9f6928
muchachooos <odnorogg@bk.ru>
2022-12-14 03:24:17 +0400

    Update init.sql

--- resources/init.sql
+++ resources/init.sql
@@ -3,7 +3,7 @@ CREATE TABLE `user`
     `id`       INT AUTO_INCREMENT PRIMARY KEY,
     `login`    VARCHAR(25) UNIQUE CHECK (`login` != '')   NOT NULL,
     `password` VARCHAR(30) CHECK ( `password` != '') NOT NULL,
-    `token`    CHAR(36),
+    `token`    CHAR(36) UNIQUE,
     `time`     DATETIME
 );
 
