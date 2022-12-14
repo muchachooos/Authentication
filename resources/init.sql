b2ac48724b18cda4c7ac5188f2779c8434dbf176
muchachooos <odnorogg@bk.ru>
2022-12-14 22:00:59 +0400

    Update init.sql

--- resources/init.sql
+++ resources/init.sql
@@ -1,16 +1,17 @@
 CREATE TABLE `user`
 (
-    `id`       INT AUTO_INCREMENT PRIMARY KEY,
-    `login`    VARCHAR(25) UNIQUE CHECK (`login` != '')   NOT NULL,
-    `password` VARCHAR(30) CHECK ( `password` != '') NOT NULL,
-    `token`    CHAR(36) UNIQUE,
-    `time`     DATETIME
+    `id`         INT AUTO_INCREMENT PRIMARY KEY,
+    `login`      VARCHAR(25) UNIQUE CHECK (`login` != '')        NOT NULL,
+    `hashPass` VARCHAR(300) UNIQUE CHECK ( `hashPass` != '') NOT NULL,
+    `token`      CHAR(36) UNIQUE,
+    `time`       DATETIME
 );
 
 DROP TABLE `user`;
 SHOW DATABASES;
 USE auth_data;
 SHOW TABLES;
-SELECT *FROM user;
+SELECT *
+FROM user;
 
 # SELECT EXISTS(SELECT * FROM user WHERE time = ?)
