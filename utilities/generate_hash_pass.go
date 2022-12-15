b3db87574b475d02b34f1ccf7881c8e2df8b7fa3
muchachooos <odnorogg@bk.ru>
2022-12-15 20:33:51 +0400

    Create generate_hash_pass.go

---
+++ utilities/generate_hash_pass.go
@@ -0,0 +1,17 @@
+package utilities
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+const cost = 10
+
+func HashingPassword(pass string) (string, error) {
+
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(pass), cost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hashedPass), nil
+}
