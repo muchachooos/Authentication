0df2a96e1cb8ae96ab2aa69074875ec6b9791f17
muchachooos <odnorogg@bk.ru>
2022-12-15 20:34:10 +0400

    Update model.go

--- model/model.go
+++ model/model.go
@@ -5,12 +5,16 @@ import (
 	"time"
 )
 
-type Data struct {
-	ID       int       `db:"id" json:"id"`
-	Login    string    `db:"login" json:"login"`
-	Password string    `db:"password" json:"password"`
-	Token    string    `db:"token" json:"token"`
-	Time     time.Time `db:"time" json:"time"`
+type User struct {
+	ID         int       `db:"id" json:"id"`
+	Login      string    `db:"login" json:"login"`
+	HashedPass string    `db:"hashPass" json:"hashedPass"`
+	Token      string    `db:"token" json:"token"`
+	Time       time.Time `db:"time" json:"time"`
+}
+
+type HashPass struct {
+	HashedPass string `db:"hashPass" json:"hashedPass"`
 }
 
 type CheckTokenResponse struct {
