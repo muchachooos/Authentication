3c33845229907fe19dad1ce1e37c2ff5093665b6
muchachooos <odnorogg@bk.ru>
2022-12-17 17:55:25 +0400

    Update init.sql

--- resources/sql/init.sql
+++ resources/sql/init.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `user`
 (
-    `id`         INT AUTO_INCREMENT PRIMARY KEY,
-    `login`      VARCHAR(25) UNIQUE CHECK (`login` != '')        NOT NULL,
+    `id`       INT AUTO_INCREMENT PRIMARY KEY,
+    `login`    VARCHAR(25) UNIQUE CHECK (`login` != '')      NOT NULL,
     `hashPass` VARCHAR(300) UNIQUE CHECK ( `hashPass` != '') NOT NULL,
-    `token`      CHAR(36) UNIQUE,
-    `time`       DATETIME
+    `token`    CHAR(36) UNIQUE,
+    `time`     DATETIME
 );
 
 DROP TABLE `user`;
